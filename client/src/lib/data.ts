794356bb77efd1285f4a6e26cc20bce73cc17979
Aarón Hernández <139735108+aarn-hs@users.noreply.github.com>
2025-06-26 22:00:09 +0000

    Add agencies sources

--- client/src/lib/data.ts
+++ client/src/lib/data.ts
@@ -1,12 +1,12 @@
 // Source agencies
 export const agencySources = [
-  { value: "walk-ins", label: "Walk-In" },
+  { value: "agencia_bbn", label: "Agencia BBN" },
+  { value: "agencia_cygnus", label: "Agencia Cygnus" },
+  { value: "agencia_hq", label: "Agencia HQ" },
+  { value: "agencia_peoplecare", label: "Agencia Peoplecare" },
+  { value: "agencia_stoneway", label: "Agencia Stoneway" },
   { value: "candidato-analogo", label: "Candidato Análogo" },
-  { value: "agencia_cygnus", label: "Agencia Cygnus"" },
-  { value: "agencia_bbn", label: "Agencia BBN"" },
-  { value: "agencia_peoplecare", label: "Agencia Peoplecare" },,
-  { value: "agencia_hq", label: "Agencia HQ },
-  { value: "agencia_stoneway", label: "Agencia Stoneway }
+  { value: "walk-ins", label: "Walk-In" }
 ];
 
 // Genders
